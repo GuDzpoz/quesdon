7c40c34082dfa911a932bb6cc2e7bbcce9fbb1f3
Cutls <web-pro@cutls.com>
2019-06-02 19:46:59 +0900

    Add warn

--- src/client/components/pages/index.tsx
+++ src/client/components/pages/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link } from "react-router-dom"
 import { me } from "../../initial-state"
 import { PageLatest } from "./latest"
+import { Alert } from "reactstrap"
 
 export class PageIndex extends React.Component {
     render() {
@@ -12,23 +13,15 @@ export class PageIndex extends React.Component {
             <p>本家(quesdon.rinsuki.net)から質問を移行できる！</p>
             <details>
                 <summary>
-                    質問にログインが要るようになりました。
-                </summary>
-                スパム排除のための処置です。ご理解お願い致します。
-            </details>
-            <details>
-                <summary>
                     これはCutls Pによる運営です。開発者の運営ではありません。
                 </summary>
-                独自機能を除く開発者様へのお問い合わせは<a href="https://mstdn.rinsuki.net/@rinsuki">@rinsuki@mstdn.rinsuki.net</a>または<a href="https://mstdn.rinsuki.net/@quesdon">@quesdon@mstdn.rinsuki.net</a>までお願いします。
+                独自機能を除く開発者様へのお問い合わせは<a href="https://mstdn.rinsuki.net/@rinsuki" target="_blank">@rinsuki@mstdn.rinsuki.net</a>または
+                <a href="https://mstdn.rinsuki.net/@quesdon" target="_blank">@quesdon@mstdn.rinsuki.net</a>までお願いします。
             </details>
-            <details>
-                <summary>
-                Quesdon(toot.app)だけの限定機能が追加！
-                </summary>
-                <li>自分宛ての新しい質問ができると<a href="https://mstdn.jp/@qtnotf">@qtnotf@mstdn.jp</a>よりDMで通知されます。要らない場合はこのアカウントをミュートしてください。</li>
-                <li>Misskeyでもログインできます。ログイン時に「Misskeyとしてログイン」にチェック！</li>
-            </details>
+            <Alert color="warning">
+                <p className="bold">なかなかにヤバいです</p>
+                運営費の捻出にご協力ください。<a href="kyash://qr/u/611373232688837504">Kyash</a>/<a href="https://www.pixiv.net/fanbox/creator/28105985" target="_blank">PixivFANBOX</a>
+            </Alert>
             <p>{me ? <Link to="/my">マイページ</Link> : <Link to="/login">ログイン</Link>}</p>
             <PageLatest />
         </div>
